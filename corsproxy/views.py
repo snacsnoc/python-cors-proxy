eee37c47653f5278d9e6a43354e233a451ab80ab
Easton Elliott <easton@geekness.eu>
2023-01-15 14:43:06 -0700

    Update invalid origin error message with the host included, update request headers

--- corsproxy/views.py
+++ corsproxy/views.py
@@ -12,13 +12,13 @@ def index(request):
 
     host = request.META.get('HTTP_ORIGIN')
     if host not in settings.ALLOWED_ORIGINS:
-        return HttpResponse('Invalid origin', status=403)
+        return HttpResponse('Invalid origin: ', host, status=403)
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) Firefox/106.0.1',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*',
         'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
+        'Accept-Encoding': 'application/gzip',
         'Accept-Language': 'en-US,en;q=0.8',
         'Connection': 'keep-alive'
     }
