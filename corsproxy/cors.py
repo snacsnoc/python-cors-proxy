8bb853d91f4153420d93d11c072cdf68aef1863b
Easton Elliott <easton@geekness.eu>
2023-01-16 23:09:27 -0700

    black formatting

--- corsproxy/cors.py
+++ corsproxy/cors.py
@@ -2,13 +2,12 @@ from django.http import HttpResponse
 
 
 class Middleware:
-
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
-        if (request.method == "OPTIONS"):
+        if request.method == "OPTIONS":
             return HttpResponse("OPTIONS")
-        response['Access-Control-Allow-Origin'] = "*"
+        response["Access-Control-Allow-Origin"] = "*"
         return response
